ea34f0cdffa7eef91d30e1d19a577c466daaca35
jacksonmeans <jrm380@case.edu>
2026-04-18 20:07:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('Hello Streamlit')
 name = st.text_input('Enter username')
-st.button('Greet')
-if st.button:
+button = st.button('Greet')
+if button:
   st.text_area('Hello, '+name+'! Welcome to Streamlit')
